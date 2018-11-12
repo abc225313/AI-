8fbd28eb1d63b367a74fd97e213b6e8eb259d046
abc225313 <41777892+abc225313@users.noreply.github.com>
2018-11-12 09:51:11 +0800

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -7,107 +8,167 @@ public class AI {
 	public void choice(int [][]cheeseborad){//<<一開始因應對手區域隨機下棋 1
 		if(c<4){
 			c=c==3?4:c;
-			cheeseborad[c][16-c]=2;
+			cheeseborad[c+1][5-c]=2;
 			c++;
 		}
 		else {
-			Attack(cheeseborad);
+			AttackAndDefend(cheeseborad, 4,2);
 		}
 		
 	}
 	
-	void Attack(int [][]cheeseborad){// 還要寫評估函數 走下3步
+	void AttackAndDefend(int [][]cheeseborad,int judgeNumber,int decideNumber){// 還要寫評估函數 走下3步
 		int c1=16;
-		int judge=0;int judge2=0;int judge3=0;int judge4=0;
+		int judge=0;int judge2=0;int judge3=0;int judge4=0;int judge5=0;int judge6=0;
 		int count =0;
+		ArrayList possible=new ArrayList();
 		loop:
 		for (int i = 0; i <17; i++) {
 			for (int j = 0; j <13; j++) {
 				judge=0;judge2=0;			
-				if(cheeseborad[i][j]==2)	judge++;			
-				if(cheeseborad[i][j+1]==2)	judge++;
-				if(cheeseborad[i][j+2]==2)	judge++;
-				if(cheeseborad[i][j+3]==2)	judge++;
-				if (cheeseborad[i][j+4]==2)	judge++;
+				if(cheeseborad[i][j]==decideNumber)	judge++;			
+				if(cheeseborad[i][j+1]==decideNumber)	judge++;
+				if(cheeseborad[i][j+2]==decideNumber)	judge++;
+				if(cheeseborad[i][j+3]==decideNumber)	judge++;
+				if (cheeseborad[i][j+4]==decideNumber)	judge++;
 				
-				if(cheeseborad[j][i]==2)	judge2++;
-				if(cheeseborad[j+1][i]==2)	judge2++;
-				if(cheeseborad[j+2][i]==2)	judge2++;
-				if(cheeseborad[j+3][i]==2)	judge2++;
-				if (cheeseborad[j+4][i]==2)	judge2++;
+				if(cheeseborad[j][i]==decideNumber)	judge2++;
+				if(cheeseborad[j+1][i]==decideNumber)	judge2++;
+				if(cheeseborad[j+2][i]==decideNumber)	judge2++;
+				if(cheeseborad[j+3][i]==decideNumber)	judge2++;
+				if (cheeseborad[j+4][i]==decideNumber)	judge2++;
 				
-				if(judge==4)
+				if(judge>=judgeNumber)
 				{
 					if(cheeseborad[i][j+4]==0){
-						cheeseborad[i][j+4]=2;
+						if(decideNumber==2){
+							cheeseborad[i][j+4]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 								
 					
-					else if(cheeseborad[i][j+3]==0){
-						cheeseborad[i][j+3]=2;
+					if(cheeseborad[i][j+3]==0){
+						if(decideNumber==2){
+							cheeseborad[i][j+3]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 									
 					
-					else if(cheeseborad[i][j+1]==0){
-						cheeseborad[i][j+1]=2;
+					if(cheeseborad[i][j+1]==0){
+						if(decideNumber==2){
+							cheeseborad[i][j+1]=2;
+							break loop;
+						}
+						else {
+							
+						}		
 						count++;
-						break loop;
+
 					}		
 					
-					else if(cheeseborad[i][j+2]==0){
-						cheeseborad[i][j+2]=2;
+					if(cheeseborad[i][j+2]==0){
+						if (decideNumber==2) {
+							cheeseborad[i][j+2]=2;
+							break loop;
+						}	
+						else {
+							
+						}
 						count++;
-						break loop;
-					}
-					
-					
-					else if(cheeseborad[i][j]==0){
-						cheeseborad[i][j]=2;
-						count++;
-						break loop;
-					}
 						
+					}
+					
+					
+					if(cheeseborad[i][j]==0){
+						if (decideNumber==2) {
+							cheeseborad[i][j]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+						
+					}				
 					
 					
 				}
-				else if (judge2==4)
+				if (judge2>=judgeNumber)
 				{		
 					if(cheeseborad[j+4][i]==0){
-						cheeseborad[j+4][i]=2;
+						if (decideNumber==2) {
+							cheeseborad[j+4][i]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;	
-						break loop;
+						
 					}
 											
 					
-					else if(cheeseborad[j+3][i]==0){
-						cheeseborad[j+3][i]=2;
+					if(cheeseborad[j+3][i]==0){
+						if (decideNumber==2) {
+							cheeseborad[j+3][i]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 					
 					
-					else if(cheeseborad[j+1][i]==0){
-						cheeseborad[j+1][i]=2;
+					if(cheeseborad[j+1][i]==0){
+						if (decideNumber==2) {
+							cheeseborad[j+1][i]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 				
 
-					else if(cheeseborad[j+2][i]==0){
-						cheeseborad[j+2][i]=2;
+					if(cheeseborad[j+2][i]==0){
+						if (decideNumber==2) {
+							cheeseborad[j+2][i]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 					
 
-					else if(cheeseborad[j][i]==0){
-						cheeseborad[j][i]=2;
+					if(cheeseborad[j][i]==0){
+						if (decideNumber==2) {
+							cheeseborad[j][i]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
 						
 					
@@ -118,92 +179,278 @@ public class AI {
 			}
 			
 		}
-		int up=12;
+		int up=12;int down=0;
 		loop:
 		for(int s=0;s<13;s++){
 			for (int f=0;f<=s;f++){	
 				judge3=0;judge4=0;
-				if(cheeseborad[up][f]==2)	judge3++;
-				if(cheeseborad[1+up][f+1]==2)	judge3++;
-				if(cheeseborad[up+2][f+2]==2)	judge3++;
-				if(cheeseborad[up+3][f+3]==2)	judge3++;
-				if(cheeseborad[up+4][f+4]==2)	judge3++;
+				if(cheeseborad[up+f][f]==decideNumber)		judge3++;
+				if(cheeseborad[up+f+1][f+1]==decideNumber)	judge3++;
+				if(cheeseborad[up+f+2][f+2]==decideNumber)	judge3++;
+				if(cheeseborad[up+f+3][f+3]==decideNumber)	judge3++;
+				if(cheeseborad[up+f+4][f+4]==decideNumber)	judge3++;
 
-				if(cheeseborad[up][c1-f]==2)	judge4++;
-				if(cheeseborad[up+1][c1-(f+1)]==2)	judge4++;
-				if(cheeseborad[up+2][c1-(f+2)]==2)	judge4++;
-				if(cheeseborad[up+3][c1-(f+3)]==2)	judge4++;
-				if(cheeseborad[up+4][c1-(f+4)]==2)	judge4++;
-				if(judge3>=3){
-					if(cheeseborad[up][f]==0){
-						cheeseborad[up][f]=2;
+				if(cheeseborad[up+f][c1-f]==decideNumber)		judge4++;
+				if(cheeseborad[up+f+1][c1-(f+1)]==decideNumber)	judge4++;
+				if(cheeseborad[up+f+2][c1-(f+2)]==decideNumber)	judge4++;
+				if(cheeseborad[up+f+3][c1-(f+3)]==decideNumber)	judge4++;
+				if(cheeseborad[up+f+4][c1-(f+4)]==decideNumber)	judge4++;
+				
+				if(cheeseborad[down-f][f+4]==decideNumber)		judge5++;
+				if(cheeseborad[down+1-f][f+3]==decideNumber)	judge5++;
+				if(cheeseborad[down+2-f][f+2]==decideNumber)	judge5++;
+				if(cheeseborad[down+3-f][f+1]==decideNumber)	judge5++;
+				if(cheeseborad[down+4-f][f]==decideNumber)		judge5++;
+				
+				if(cheeseborad[down-f][c1-(f+4)]==decideNumber)	judge6++;
+				if(cheeseborad[down+1-f][c1-(f+3)]==decideNumber)	judge6++;
+				if(cheeseborad[down+2-f][c1-(f+2)]==decideNumber)	judge6++;
+				if(cheeseborad[down+3-f][c1-(f+1)]==decideNumber)	judge6++;
+				if(cheeseborad[down+4-f][c1-f]==decideNumber)		judge6++;
+				//
+				if(judge3>=judgeNumber){
+					if(cheeseborad[up+f][f]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f][f]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 						
 					}
-					else if(cheeseborad[up+1][f+1]==0){
-						cheeseborad[up+1][f+1]=2;
+					if(cheeseborad[up+f+1][f+1]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+1][f+1]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+2][f+2]==0){
-						cheeseborad[up+2][f+2]=2;
+					if(cheeseborad[up+f+2][f+2]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+2][f+2]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+3][f+3]==0){
-						cheeseborad[up+3][f+3]=2;
+					if(cheeseborad[up+f+3][f+3]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+3][f+3]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+4][f+4]==0){
-						cheeseborad[up+4][f+4]=2;
+					if(cheeseborad[up+f+4][f+4]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+4][f+4]=2;
+							break loop;
+						}
+						else {
+							
+						}		
 						count++;
-						break loop;
+						
 					}
 					
 				}
-				else if (judge4>=3)//目前這裡有問題
+				if (judge4>=judgeNumber)//目前這裡有問題
 				{
-					if(cheeseborad[up][c1-f]==0){
-						cheeseborad[up][c1-f]=2;
+					if(cheeseborad[up+f][c1-f]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f][c1-f]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+1][c1-(f+1)]==0){
-						cheeseborad[up+1][c1-(f+1)]=2;
+					if(cheeseborad[up+f+1][c1-(f+1)]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+1][c1-(f+1)]=2;
+							break loop;
+						}
+						else{
+							
+						}			
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+2][c1-(f+2)]==0){
-						cheeseborad[up+2][c1-(f+2)]=2;
+					if(cheeseborad[up+f+2][c1-(f+2)]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+2][c1-(f+2)]=2;
+							break loop;
+						}
+						else {
+							
+						}					
 						count++;
-						break loop;
+						
 					}
-					else if(cheeseborad[up+3][c1-(f+3)]==0){
-						cheeseborad[up+3][c1-(f+3)]=2;
+					if(cheeseborad[up+f+3][c1-(f+3)]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+3][c1-(f+3)]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
+
 					}
-					else if(cheeseborad[up+4][c1-(f+4)]==0){
-						cheeseborad[up+4][c1-(f+4)]=2;
+					if(cheeseborad[up+f+4][c1-(f+4)]==0){
+						if (decideNumber==2) {
+							cheeseborad[up+f+4][c1-(f+4)]=2;
+							break loop;
+						}
+						else {
+							
+						}
 						count++;
-						break loop;
 					}
 					
 				}
-				
-			
-				
+				//
+				if (judge5>=judgeNumber)
+				{
+					if(cheeseborad[down-f][f+4]==0){
+						if (decideNumber==2) {
+							cheeseborad[down-f][f+4]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+1-f][f+3]==0){
+						if (decideNumber==2) {
+							cheeseborad[down+1-f][f+3]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+2-f][f+2]==0){
+						if (decideNumber==2) {
+							cheeseborad[down+2-f][f+2]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+3-f][f+1]==0){
+						if (decideNumber==2) {
+							cheeseborad[down+3-f][f+1]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+
+					}
+					if(cheeseborad[down+4-f][f]==0){
+						if (decideNumber==2) {
+							cheeseborad[down+4-f][f]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					
+				}
+				//
+				if(judge6>=judgeNumber){
+					if(cheeseborad[down-f][c1-(f+4)]==0){
+						if(decideNumber==2){
+							cheeseborad[down-f][c1-(f+4)]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+1-f][c1-(f+3)]==0){
+						if(decideNumber==2){
+							cheeseborad[down+1-f][c1-(f+3)]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+2-f][c1-(f+2)]==0){
+						if(decideNumber==2){
+							cheeseborad[down+2-f][c1-(f+2)]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+3-f][c1-(f+1)]==0){
+						if(decideNumber==2){
+							cheeseborad[down+3-f][c1-(f+1)]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+					}
+					if(cheeseborad[down+4-f][c1-f]==0){
+						if(decideNumber==2){
+							cheeseborad[down+4-f][c1-f]=2;
+							break loop;
+						}
+						else {
+							
+						}
+						count++;
+
+					}
+					
+				}
 				
 				
 
 			}
-			up--;//--代表往上提升
+			up--;down++;//--代表往上提升
 			
 		}
-		if (count==0) {
-			Defend(cheeseborad);
+		if (count==0&&decideNumber==2) {
+			AttackAndDefend(cheeseborad, 3,1);//防守
+		}
+		else if (count>0&&decideNumber==1) {
+			// A* MINMAX
+		}
+		else if (count==0&&decideNumber==1) {
+			//把所有可防守性丟到minmax去評估最佳解
 		}
 		
 		
