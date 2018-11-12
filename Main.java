7172f73a3d131b355e3e450bd66485117599644e
abc225313 <41777892+abc225313@users.noreply.github.com>
2018-11-12 09:51:33 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,10 +7,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 
-public class Execute {
+public class Execute {// A* ab cut minmax
 	private int [][]cheeseborad=new int[17][17];
 	private JFrame jframe;
 	private AI ai;
+	private static int count=0;
 
 	/**
 	 * Launch the application.
@@ -57,26 +58,55 @@ public class Execute {
 		                        	for (int j = 0; j < button.length; j++) {
 		                        		for (int j1 = 0; j1 < button.length; j1++) {
 		                        			if(check(cheeseborad)){break loop;}
-			                        		if (e.getSource() == button[j][j1])
-								{
-			                        			if(cheeseborad[j][j1]==0){
+		                        			if (e.getSource() == button[j][j1]){
+		                        				if(cheeseborad[j][j1]==0){
+			                        				count++;
 			                        				cheeseborad[j][j1]=1;
 				                        			button[j][j1].setLabel("O");
-				                        			cheeseborad=ai.choice(cheeseborad);
+				                        			if(check(cheeseborad)){break loop;}
+				                        			ai.choice(cheeseborad);
+				                        			for (int k = 0; k < button.length; k++) {
+														for (int l = 0; l < button.length; l++) {
+															if(cheeseborad[k][l]==2){
+																button[k][l].setLabel("X");
+															}
+															
+														}
+														
+													}
 			                        			}
 			                        			else {
-										System.out.print("It is not empty\n");
-									}
-			                        			
-			                        			
-
-			                        
+													System.out.print("It is not empty\n");
+												}
+		                        			}
+		                        				
+		                        			}
+		                        		}
+		                        			
+		                        	/*loop:
+		                        	for (int j = 0; j < button.length; j++) {
+		                        		for (int j1 = 0; j1 < button.length; j1++) {
+		                        			if(check(cheeseborad)){break loop;}
+		                        			
+		                        				if (e.getSource() == button[j][j1]){
+				                        			if(cheeseborad[j][j1]==0){
+				                        				count++;
+				                        				cheeseborad[j][j1]=1;
+					                        			button[j][j1].setLabel("O");
+					                        			if(check(cheeseborad)){break loop;}
+				                        			}
+				                        			else {
+														System.out.print("It is not empty\n");
+													}
+		                        			}
+			                         
 				                        		
-				                        	}
+				                        	
+		                        			
 											
 										}
 										
-									}
+									}*/
 
 		                        }
 		                            
@@ -119,27 +149,51 @@ public class Execute {
 			}
 			
 		}
-		int count=0;
-		int up=12;
-		for(int s=0;s<13;s++){
-			for (int f=0;f<=count;f++){	
-				if(cheeseborad[up][f]==cheeseborad[1+up][f+1]&&cheeseborad[up][f]==cheeseborad[2+up][f+2]&&cheeseborad[up][f]==cheeseborad[up+3][f+3]&&cheeseborad[up][f]==cheeseborad[up+4][f+4]){
-					if(cheeseborad[up][f]==1){
+		
+		int up=12;int down=0;
+		for(int s=0;s<13;s++){//4個
+			for (int f=0;f<=s;f++){	
+				if(cheeseborad[up+f][f]==cheeseborad[up+1+f][f+1]&&cheeseborad[up+f][f]==cheeseborad[up+2+f][f+2]&&cheeseborad[up+f][f]==cheeseborad[up+3+f][f+3]&&cheeseborad[up+f][f]==cheeseborad[up+4+f][f+4]){
+					
+					if(cheeseborad[up+f][f]==1){
 						System.out.print("Player win!\n");
 						return true;
 					}
-					else if (cheeseborad[up][f]==2){
+					else if (cheeseborad[up+f][f]==2){
 						System.out.print("AI win!\n");
 						return true;
 					}
 
 				}
-				if(cheeseborad[up][c1-f]==cheeseborad[up+1][c1-(f+1)]&&cheeseborad[up][c1-f]==cheeseborad[up+2][c1-(f+2)]&&cheeseborad[up][c1-f]==cheeseborad[up+3][c1-(f+3)]&&cheeseborad[up][c1-f]==cheeseborad[up+4][c1-(f+4)]){
-					if(cheeseborad[up][c1-f]==1){
+				if(cheeseborad[up+f][c1-f]==cheeseborad[up+f+1][c1-(f+1)]&&cheeseborad[up+f][c1-f]==cheeseborad[up+f+2][c1-(f+2)]&&cheeseborad[up+f][c1-f]==cheeseborad[up+f+3][c1-(f+3)]&&cheeseborad[up+f][c1-f]==cheeseborad[up+f+4][c1-(f+4)]){
+					if(cheeseborad[up+f][c1-f]==1){
 						System.out.print("Player win!\n");
 						return true;
 					}
-					else if (cheeseborad[up][c1-f]==2){
+					else if (cheeseborad[up+f][c1-f]==2){
+						System.out.print("AI win!\n");
+						return true;
+					}
+
+				}
+
+				if(cheeseborad[down-f][f+4]==cheeseborad[down+1-f][f+3]&&cheeseborad[down-f][f+4]==cheeseborad[down+2-f][f+2]&&cheeseborad[down-f][f+4]==cheeseborad[down+3-f][f+1]&&cheeseborad[down-f][f+4]==cheeseborad[down+4-f][f]){		
+					if(cheeseborad[down-f][f+4]==1){
+						System.out.print("Player win!\n");
+						return true;
+					}
+					else if (cheeseborad[down-f][f+4]==2){
+						System.out.print("AI win!\n");
+						return true;
+					}
+
+				}
+				if(cheeseborad[down-f][c1-(f+4)]==cheeseborad[down+1-f][c1-(f+3)]&&cheeseborad[down-f][c1-(f+4)]==cheeseborad[down+2-f][c1-(f+2)]&&cheeseborad[down-f][c1-(f+4)]==cheeseborad[down+3-f][c1-(f+1)]&&cheeseborad[down-f][c1-(f+4)]==cheeseborad[down+4-f][c1-f]){		
+					if(cheeseborad[down-f][c1-(f+4)]==1){
+						System.out.print("Player win!\n");
+						return true;
+					}
+					else if (cheeseborad[down-f][c1-(f+4)]==2){
 						System.out.print("AI win!\n");
 						return true;
 					}
@@ -149,8 +203,9 @@ public class Execute {
 				
 
 			}
-			count++;up--;//--代表往上提升
+			up--;down++;//--代表往上提升
 			
+
 		}
 		
 		
